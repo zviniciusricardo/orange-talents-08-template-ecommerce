8581cb87570391995336263f957aff2bf8378c81
viniciusferreira <vinicius.ricardo@zup.com.br>
2021-09-14 17:51:12 -0300

    resolvendo os problemas de dependência no Intellij

--- mercadolivre/src/main/java/br/com/zupacademy/vinicius/mercadolivre/MercadoLivreApplication.java
+++ mercadolivre/src/main/java/br/com/zupacademy/vinicius/mercadolivre/MercadoLivreApplication.java
@@ -2,9 +2,7 @@ package br.com.zupacademy.vinicius.mercadolivre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableFeignClients
